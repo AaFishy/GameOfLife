104e71c7443afd403ed6a74555eeb1944c85b2b9
Aaron Lin <aaronlin33@gmail.com>
2020-07-23 22:00:26 +1000

    Added base for game of life

--- src/unsw/automata/GameOfLife.java
+++ src/unsw/automata/GameOfLife.java
@@ -3,6 +3,11 @@
  */
 package unsw.automata;
 
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.beans.property.BooleanProperty;
+
 /**
  * Conway's Game of Life on a 10x10 grid.
  *
@@ -10,9 +15,18 @@ package unsw.automata;
  *
  */
 public class GameOfLife {
+    private GridPane grid = new GridPane();
 
     public GameOfLife() {
         // TODO At the start all cells are dead
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
+                CheckBox newCheck = new CheckBox();
+                newCheck.setIndeterminate(false);
+                newCheck.setSelected(false);
+                grid.add(newCheck, x, y);
+            }
+        }
     }
 
     public void ensureAlive(int x, int y) {
