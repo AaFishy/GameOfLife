5d7265f19b151ba55f7b0fde1c44589554374034
aaron lin <y.j.lin@student.unsw.edu.au>
2020-07-24 02:09:52 +1000

    Finished backend

--- src/unsw/automata/GameOfLife.java
+++ src/unsw/automata/GameOfLife.java
@@ -15,35 +15,84 @@ import javafx.beans.property.BooleanProperty;
  *
  */
 public class GameOfLife {
-    private GridPane grid = new GridPane();
-
+    //private GridPane grid = new GridPane();
+    private boolean[][] matrix = new boolean[10][10];
     public GameOfLife() {
         // TODO At the start all cells are dead
         for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
+                matrix[x][y] = false;
+            }
+        }
+        /*for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 10; y++) {
                 CheckBox newCheck = new CheckBox();
                 newCheck.setIndeterminate(false);
                 newCheck.setSelected(false);
                 grid.add(newCheck, x, y);
             }
-        }
+        }*/
     }
 
     public void ensureAlive(int x, int y) {
         // TODO Set the cell at (x,y) as alive
+        matrix[x][y] = true;
     }
 
     public void ensureDead(int x, int y) {
         // TODO Set the cell at (x,y) as dead
+        matrix[x][y] = false;
     }
 
     public boolean isAlive(int x, int y) {
         // TODO Return true if the cell is alive
-        return false;
+        if (matrix[x][y]) return true;
+        else return false;
+    }
+
+    private int countNeighbours(int x, int y) {
+        int numNeighbours = 0;
+
+        int leftX = (x - 1 < 0) ? (x + 10) : (x - 1);
+        int rightX = (x + 1 > 9) ? (x - 10) : (x + 1);
+        int upY = (y - 1 < 0) ? (y + 10) : (y - 1);
+        int downY = (y + 1 > 9) ? (y - 10) : (y + 1);
+
+        if (matrix[leftX][y]) numNeighbours++;
+        if (matrix[leftX][upY]) numNeighbours++;
+        if (matrix[leftX][downY]) numNeighbours++;
+        if (matrix[rightX][y]) numNeighbours++;
+        if (matrix[rightX][upY]) numNeighbours++;
+        if (matrix[rightX][downY]) numNeighbours++;
+        if (matrix[x][upY]) numNeighbours++;
+        if (matrix[x][downY]) numNeighbours++;
+
+        return numNeighbours;
     }
 
     public void tick() {
         // TODO Transition the game to the next generation.
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
+                int numNeighbours = countNeighbours(x,y);
+                if (matrix[x][y]) {
+                    switch (numNeighbours) {
+                        case 0:
+                        case 1:
+                            matrix[x][y] = false;
+                            break;
+                        case 2:
+                        case 3:
+                            matrix[x][y] = true;
+                            break;
+                        default:
+                            matrix[x][y] = false;
+                    }
+                } else {
+                    if (numNeighbours == 3) matrix[x][y] = true;
+                }
+            }
+        }
     }
 
 }
